6b892b9be096a89af269518867d2995fd0a308fb
SunYanfa <144716653+SunYanfa@users.noreply.github.com>
2023-10-31 13:18:24 +0100

    Add files via upload

--- passBanner.sh
+++ passBanner.sh
@@ -2,27 +2,27 @@
 
 # todas las palabras contengan sólamente letras minusculas
 
-grep -oE '\b[a-z]+\b' Banner.html | sort | uniq > ./banner/allminus.txt
+grep -oE '\b[a-z]+\b' Banner.html | sort | uniq > allminus.txt
 
 
 
 # amplificar el diccionario de passwords con la opcion rule de john
 
-john --stdout --wordlist=./banner/allminus.txt --rule > ./banner/rule.txt
+john --stdout --wordlist=allminus.txt --rule > rule.txt
 
 
 
 # se eliminarán las palabras que no contengan todo minúsculas
 
-grep -oE '\b[A-Za-z]*[A-Z]+[A-Za-z]*\b' ./banner/rule.txt > ./banner/mayuscula.txt
+grep -oE '\b[A-Za-z]*[A-Z]+[A-Za-z]*\b' rule.txt > mayuscula.txt
 
 
 # se eliminarán las palabras que no sean menores a 8 caracteres
 
-grep -oE '\b\w{1,7}\b' ./banner/mayuscula.txt > ./banner/menor8.txt
+grep -oE '\b\w{1,7}\b' mayuscula.txt > menor8.txt
 
 
 
 # reordenar las palabras restantes y eliminar duplicados
 
-sort -u ./banner/menor8.txt > ./banner/pass.txt
+sort -u menor8.txt > pass.txt
